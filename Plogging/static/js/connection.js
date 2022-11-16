cd550fde47b7ff17a5d482aa06e6232344360f1c
LinkState <jjyyhh99@naver.com>
2022-11-17 02:48:32 +0900

    수정
    주석 위치 변경

--- Plogging/static/js/connection.js
+++ Plogging/static/js/connection.js
@@ -10,8 +10,8 @@ SOCKET.on('response', function(data) {
             let day = data['data']['startPloggingTime']['d'];
             let hour = data['data']['startPloggingTime']['h'];
             let min = data['data']['startPloggingTime']['m'];
-            // START_TIME = new Date(year, month-1, day, hour, min).getTime();
-            START_TIME = Date.now()+5000;
+            START_TIME = new Date(year, month-1, day, hour, min).getTime();
+            // START_TIME = Date.now()+5000;
             loadPloggingTimer();
         }
     }
