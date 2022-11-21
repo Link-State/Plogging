38b21b00d19491cdf275c876a55edc4ca6906a39
DrNarae <jjyyhh99@naver.com>
2022-11-22 08:47:28 +0900

    팝업창 디자인

--- Plogging/static/js/popup.js
+++ Plogging/static/js/popup.js
@@ -1,9 +1,16 @@
 // 팝업메세지창
 function actionMessage(msg) {
     alert(msg);
+
+    let popupWindow = document.createElement("div");
+    let popupText = document.createElement("span");
+    let popupCheck = document.createElement("div");
+
+    // 띄우고 3초 뒤에 사라짐
 }
 
 // 선택창
 function confirmWindow(msg) {
     
 }
+
